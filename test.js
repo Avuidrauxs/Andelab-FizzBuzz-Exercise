facfdced189d9ef19132ffc450cc2ca707165e83
Audax Faarentie Anchirinah <audax@localhost.localdomain>
2017-02-08 22:12:53 +0100

    Created tests to assert my program

---
+++ test.js
@@ -0,0 +1,47 @@
+
+var myApp = require('./app.js');
+
+describe("Fizz Buzz tests ", function() {
+
+  it("should return `Fizz` for number divisible by 3", function() {
+    expect(myApp.fizzBuzz(3)).toBe('Fizz');
+  });
+
+  it("should return `Buzz` for number divisible by 5", function() {
+    expect(myApp.fizzBuzz(5)).toBe('Buzz');
+  });
+
+  it("should return `FizzBuzz` for 15", function() {
+    expect(myApp.fizzBuzz(15)).toBe('FizzBuzz');
+  });
+
+  it("should return `FizzBuzz` for 45", function() {
+    expect(myApp.fizzBuzz(45)).toBe('FizzBuzz');
+  });
+
+  it("should return `FizzBuzz` for 90", function() {
+    expect(myApp.fizzBuzz(90)).toBe('FizzBuzz');
+  });
+
+  it("should return `Fizz` for 63", function() {
+    expect(myApp.fizzBuzz(63)).toBe('Fizz');
+  });
+
+  it("should return 7 since its indivisible by 3 and 5", function() {
+    expect(myApp.fizzBuzz(7)).toBe(7);
+  });
+
+  it("should return 101 since its indivisible by 3 and 5", function() {
+    expect(myApp.fizzBuzz(101)).toBe(101);
+  });
+
+  //Included my own tests
+    it("should return 'Fizz' for 69", function() {
+    expect(myApp.fizzBuzz(69)).toBe('Fizz');
+  });
+
+  it("should return 'Undefined' since its indivisible by 3 and 5", function() {
+    expect(myApp.fizzBuzz(0)).toBe("Undefined");
+  });
+
+});
